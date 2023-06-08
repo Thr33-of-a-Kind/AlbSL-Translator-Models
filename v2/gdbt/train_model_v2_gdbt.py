2f12fb7c95e4a2a38c7736f4703209fa988e9b8c
unknown <joanajaupi01@gmail.com>
2023-06-09 01:30:58 +0200

    added classification report for xgboost

--- v2/gdbt/train_model_v2_gdbt.py
+++ v2/gdbt/train_model_v2_gdbt.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, \
-    ConfusionMatrixDisplay
+    ConfusionMatrixDisplay, classification_report
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 
@@ -23,7 +23,7 @@ accuracy_scores = []
 precision_scores = []
 recall_scores = []
 f1_scores = []
-
+classification_reports = []
 best_score = -1
 model = XGBClassifier(n_estimators=2, max_depth=6, learning_rate=1, objective="binary:logistic")
 for _ in range(num_iterations):
@@ -36,6 +36,8 @@ for _ in range(num_iterations):
     precision = precision_score(y_test, y_predict, average='macro')
     recall = recall_score(y_test, y_predict, average='macro')
     f1 = f1_score(y_test, y_predict, average='macro')
+    classification = classification_report(y_test, y_predict)
+
 
 
     if best_score < accuracy:
@@ -48,6 +50,8 @@ for _ in range(num_iterations):
     precision_scores.append(precision)
     recall_scores.append(recall)
     f1_scores.append(f1)
+    classification_reports.append(classification)
+
 
 average_accuracy = np.mean(accuracy_scores)
 average_precision = np.mean(precision_scores)
@@ -60,6 +64,11 @@ with open('metrics.txt', 'w') as f:
     f.write('Average precision: {:.2%}\n'.format(average_precision))
     f.write('Average recall: {:.2%}\n'.format(average_recall))
     f.write('Average F1 score: {:.2%}\n'.format(average_f1))
+    f.write('\n--- Classification Reports ---\n\n')
+    for i, report in enumerate(classification_reports):
+        f.write(f'Iteration {i + 1}:\n')
+        f.write(report)
+        f.write('\n\n')
 
 
 
@@ -72,7 +81,7 @@ plt.ylabel('Score')
 plt.savefig('metrics_boxplot.png')
 plt.close()
 
-print('Confusion matrix of the iteration with best accuracy')
+
 confusion_matrix_display = ConfusionMatrixDisplay.from_predictions(best_y_test, best_y_predict)
 
 # Plot the confusion matrix
@@ -80,7 +89,7 @@ fig, ax = plt.subplots(figsize=(8, 6))
 confusion_matrix_display.plot(ax=ax)
 
 # Add a title and axis labels
-plt.title('Confusion Matrix')
+plt.title('Confusion matrix of the iteration with best accuracy')
 plt.xlabel('Predicted')
 plt.ylabel('True')
 
